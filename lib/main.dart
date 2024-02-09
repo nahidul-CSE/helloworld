39d5d52e4f14affa1ad4ea8e07c7817c2ca92b78
nahidul-cse <nahidul11708033@stud.cou.ac.bd>
2024-02-09 20:08:28 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      body: Center(child: Text('Hello world 2')),
+    );
+    throw UnimplementedError();
+  }
+}
