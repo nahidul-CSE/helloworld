c0bda5c030165d313947cfac5ba02e08fe896740
nahidul-cse <nahidul11708033@stud.cou.ac.bd>
2024-02-09 23:12:53 +0600

    text

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreen,
-      body: Center(child: Text('Hello world 2')),
+      backgroundColor: Colors.blueGrey,
+      body: Center(child: Text('Hello world Nahid Hello world Nahid,Hello world Nahid,Hello world Nahid,Hello world Nahid,',
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.lightGreenAccent,
+          backgroundColor: Color(0xffd43737),
+          letterSpacing: 4,
+        ),
+        textAlign: TextAlign.center,
+        maxLines: 2,
+        overflow: TextOverflow.clip,
+
+        )),
     );
     throw UnimplementedError();
   }
