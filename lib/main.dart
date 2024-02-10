9b53b33460ce0e9e094c4d23dc9ffeea088aecb1
nahidul-cse <nahidul11708033@stud.cou.ac.bd>
2024-02-10 11:41:51 +0600

    appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -17,6 +18,15 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        leading: Icon(Icons.home, color: Colors.white54,),
+        title: Text('First App', style: TextStyle(
+          color: Colors.white70,
+        ),),
+        backgroundColor: Colors.black,
+      ),
+
+      /*
       body: Center(child: Text('Hello world Nahid Hello world Nahid,Hello world Nahid,Hello world Nahid,Hello world Nahid,',
         style: TextStyle(
           fontSize: 20,
@@ -28,7 +38,18 @@ class HomeScreen extends StatelessWidget {
         maxLines: 2,
         overflow: TextOverflow.clip,
 
-        )),
+        ),
+
+      ),*/
+      /*body: Image.network('https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg',
+      ),*/
+      body: Center(
+          child: Image.asset(
+        'images/cse.png',
+        height: 300,
+        width: 300,
+        fit: BoxFit.contain,
+      )),
     );
     throw UnimplementedError();
   }
