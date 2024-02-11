e480cfb1cbd61ff8a5d28c7ca79ae8e6c130384f
nahidul-cse <nahidul11708033@stud.cou.ac.bd>
2024-02-11 12:58:05 +0600

    richtext

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class HomeScreen extends StatelessWidget {
       body: Row(
         //mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        //crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('Column'),
@@ -57,6 +57,49 @@ class HomeScreen extends StatelessWidget {
                   );
                 });
               }, child: Text('Click')),
+      /*showModalBottomSheet(context: context, builder: (context){
+        return Column(
+          children: [
+            Text('Text from bottom Sheet')
+          ],
+        ),
+      });*/
+              ElevatedButton(onPressed: (){
+                showModalBottomSheet(
+                  isDismissible: false,
+                    backgroundColor: Colors.lightBlue,
+                    context: context, builder: (context){
+                  return Column(
+                    children: [
+                      Text('Text from Modal Sheet11111111111111111111111',),
+                      Text('Text from Modal Sheet 2'),
+                      RichText(text: TextSpan(
+                        text: 'Please Accept the ',
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Terms',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 20,
+                            ),
+                          ),
+                          TextSpan(
+                            text: '& Conditions',
+                            style: TextStyle(
+                              color: Colors.white54,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+
+                      ),),
+                    ],
+                  );
+                });
+              }, child: Text('Tap')),
 
             ],
           ),
